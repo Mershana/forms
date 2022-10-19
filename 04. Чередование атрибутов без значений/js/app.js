a3997f3b502698d384fa27c324a7c0a5e3f0b366
Mershana <kriomaas@gmail.com>
2022-10-19 17:19:05 +0300

    04. Чередование атрибутов без значений

---
+++ 04. Чередование атрибутов без значений/js/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Чередование атрибутов без значений
+/* №2⊗jsPmFmAWVA
+Дан чекбокс и кнопка. Сделайте так, чтобы каждый клик по кнопке приводил к смене состояния чекбокса. */
+/* let elems = document.querySelector('#elems');
+let button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+	elems.checked = !elems.checked
+}) */
+
