aada2d2589147bcde2cc6a3d21a7fc8d2f23e4a1
Mershana <kriomaas@gmail.com>
2022-10-19 17:18:23 +0300

    01. Работа с текстареа

---
+++ 01. Работа с текстареа/js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Работа с текстареа в JavaScript
+/* №1⊗jsPmFmTa
+Дан текстареа и абзац. По потери фокуса в текстареа запишите его текст в абзац. */
+/* let elem = document.querySelector('#elem');
+let paragraph = document.querySelector('p')
+elem.addEventListener('blur', function(){
+	paragraph.textContent = elem.value;
+}) */
+
