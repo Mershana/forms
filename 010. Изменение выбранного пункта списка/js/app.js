2c9463670a23c3434a7ba95cfdffa848f69c49bc
Mershana <kriomaas@gmail.com>
2022-10-21 14:02:51 +0300

    010. Изменение выбранного пункта списка

---
+++ 010. Изменение выбранного пункта списка/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Изменение выбранного пункта списка на JavaScript
+/* №1⊗jsPmFmSICh
+Сделайте выпадающий список с названиями месяцев. Сделайте так, чтобы JavaScript по умолчанию выбирал в этом списке текущий месяц. */
+/* let select = document.querySelector("#select");
+let presentMonth = new Date().getMonth()+1;
+
+select.addEventListener("click", function () {
+	select.value = presentMonth;
+});
+ */
