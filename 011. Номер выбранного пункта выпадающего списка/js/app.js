150d6f2050de804fa5052c2d817bc6196143b373
Mershana <kriomaas@gmail.com>
2022-10-21 14:03:07 +0300

    011. Номер выбранного пункта выпадающего списка

---
+++ 011. Номер выбранного пункта выпадающего списка/js/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// Номер выбранного пункта выпадающего списка
+/* №1⊗jsPmFmSIN
+Дан инпут и селект. В инпут вводится какое-то число. По потери фокуса сделайте выделенным пункт списка, номер которого равен значению из инпута. */
+/* let input = document.querySelector('#input');
+let select = document.querySelector('#select');
+
+input.addEventListener('blur', function(){
+	select.selectedIndex = input.value;
+});
+ */
+
+/* №2⊗jsPmFmSIN
+Сделайте выпадающий список с названиями дней недели. Сделайте так, чтобы по JavaScript по умолчанию выбирал в этом списке текущий день.
+ */
+/* let select = document.querySelector('#select');
+
+select.addEventListener('click', function(){
+	select.selectedIndex = 2;
+}) */
+
