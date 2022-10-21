48e5eedb5bbb1a64df79985ff1dbca5a7b8185c7
Mershana <kriomaas@gmail.com>
2022-10-21 14:03:27 +0300

    012. Получение пунктов выпадающего списка

---
+++ 012. Получение пунктов выпадающего списка/js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Получение пунктов выпадающего списка на JavaScript
+/* №1⊗jsPmFmDLIG */
+/* Переберите все пункты списка циклом и в конец текста каждого пункта добавьте значение его атрибута value. */
+/* let select = document.querySelector('#select');
+for (let option of select){
+	option.innerHTML += ' ' + option.value;
+} */
